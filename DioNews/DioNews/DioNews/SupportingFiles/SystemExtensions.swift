7d3fea597d9ca568f1aca1b599e4a34dc0a6d22b
Kauê Jacyntho <kauekaj@gmail.com>
2022-07-09 20:00:34 -0300

    Refatorando e melhorando o layout do App

--- DioNews/DioNews/DioNews/SupportingFiles/SystemExtensions.swift
+++ DioNews/DioNews/DioNews/SupportingFiles/SystemExtensions.swift
@@ -39,8 +39,11 @@ extension UIImageView {
         }.resume()
     }
     
-    func loadImage(from link: String?, contentMode mode: ContentMode = .scaleAspectFit) {
-        guard let link = link, let url = URL(string: link) else { return }
+    func loadImage(from link: String?, contentMode mode: ContentMode = .scaleAspectFill) {
+        guard let link = link, let url = URL(string: link) else {
+            self.image = UIImage(named: "no-image.png")
+            return
+        }
         download(from: url, contentMode: contentMode)
     }
 }
